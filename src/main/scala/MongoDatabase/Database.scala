a4365d63149d59177ed902c6ad46a07a57296e66
Ezechib <blake-96@hotmail.co.uk>
2020-03-10 12:26:11 +0000

    Modified version

--- src/main/scala/MongoDatabase/Database.scala
+++ src/main/scala/MongoDatabase/Database.scala
@@ -2,13 +2,15 @@ package MongoDatabase
 import MongoDatabase.Helpers._
 import org.mongodb.scala._
 
+import org.mongodb.scala.model.Filters._
+import org.mongodb.scala.model.Updates._
 object Database extends App {
 
   val mongoClient: MongoClient = MongoClient()
   val database: MongoDatabase = mongoClient.getDatabase("mydb")
   val collection: MongoCollection[Document] = database.getCollection("test")
 
-  val doc: Document = Document("_id" -> 0, "name" -> "MongoDB", "type" -> "database",
+  val doc: Document = Document( "name" -> "MongoDB", "type" -> "database",
     "count" -> 1, "info" -> Document("x" -> 203, "y" -> 102))
 
   collection.insertOne(doc).results()
@@ -22,5 +24,16 @@ object Database extends App {
     override def onError(e: Throwable): Unit = println("Failed")
 
     override def onComplete(): Unit = println("Completed")
+   // collection.deleteOne(equal("i", 5)).printHeadResult("Delete Result: ")
+
+   // collection.deleteMany(gte("name", "MongoDB")).printHeadResult("Delete Result: ")
+    collection.find().first().printHeadResult()
+
+    collection.find().printResults()
+
+  collection.updateOne(equal("name", "MongoDB"), set("name", "sql")).printHeadResult("Update Result: ")
+//
+//    collection.deleteOne(equal("i", 4)).printHeadResult("Delete Result: ")
+
   })
 }
